73c37255a9f1837e0816498372cb10f08ed2e569
Masami-0602 <f.m.f.3.1415926535@gmail.com>
2025-11-01 20:32:30 +0900

    フォーマッターによる修正

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,17 +1,35 @@
-import '@mui/material/styles';
+import "@mui/material/styles";
 
-declare module '@mui/material/styles' {
+declare module "@mui/material/styles" {
   interface TypographyVariants {
     weddingTitle: React.CSSProperties;
+    invitationTitle: React.CSSProperties;
+    XS: React.CSSProperties;
+    S: React.CSSProperties;
+    M: React.CSSProperties;
+    L: React.CSSProperties;
+    XL: React.CSSProperties;
   }
 
   interface TypographyVariantsOptions {
     weddingTitle?: React.CSSProperties;
+    invitationTitle: React.CSSProperties;
+    XS: React.CSSProperties;
+    S: React.CSSProperties;
+    M: React.CSSProperties;
+    L: React.CSSProperties;
+    XL: React.CSSProperties;
   }
 }
 
-  declare module '@mui/material/Typography' {
-    interface TypographyPropsVariantOverrides {
-      weddingTitle: true;
-    }
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    weddingTitle: true;
+    invitationTitle: true;
+    XS: true;
+    S: true;
+    M: true;
+    L: true;
+    XL: true;
   }
+}
--- src/theme.ts
+++ src/theme.ts
@@ -4,9 +4,29 @@ export const theme = createTheme({
   typography: {
     fontFamily: "'Noto Serif JP', serif",
     weddingTitle: {
-      fontSize:  "45px",
+      fontSize: "45px",
       fontWeight: 400,
       fontFamily: "Hafidz, sans-serif",
     },
-  }
-})
+    invitationTitle: { fontSize: "16px", fontWeight: 600 },
+    XS: { fontSize: "13px", lineHeight: "15px" },
+    S: { fontSize: "14px", lineHeight: "24px" },
+    M: { fontSize: "16px", lineHeight: "24px", fontWeight: 500 },
+    L: { fontSize: "18px", lineHeight: "24px", fontWeight: 500 },
+    XL: { fontSize: "28px", lineHeight: "41px", fontWeight: 600 },
+  },
+  palette: {
+    primary: {
+      main: "#132B36",
+      light: "#FFF",
+      dark: "#d2a14a",
+    },
+    secondary: {
+      main: "#DF8138",
+      dark: "#E6D4C0",
+    },
+    text: {
+      primary: "#132B36",
+    },
+  },
+});
